a8f3bb017dbdd9faa3ea5adb38fb3619b5c5d054
GZimmer <kristianz1000@gmail.com>
2024-02-23 11:40:57 +0100

    Update from Laptop

---
+++ AdvancedProgrammering/L4AP/src/main/java/myset/MySet.java
@@ -0,0 +1,21 @@
+package myset;
+
+public interface MySet <E>{
+  /** Remove all elements from this set */
+  void clear();
+
+    /** Return true if the element is in the set */
+    boolean contains(E e);
+
+    /** Add an element to the set */
+    boolean add(E e);
+
+    /** Remove the element from the set */
+    boolean remove(E e);
+
+    /** Return true if the set contains no elements */
+    boolean isEmpty();
+    /** Return the number of elements in the set */
+    int size();
+
+}
