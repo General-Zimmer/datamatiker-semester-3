c1550117ad2b073c98ac8425ba75a259b17132d1
GZimmer <kristianz1000@gmail.com>
2024-05-01 08:59:34 +0200

    Update from Laptop

--- AdvancedProgrammering/L8AP/src/main/java/exercises1/Exercises1.java
+++ AdvancedProgrammering/L8AP/src/main/java/exercises1/Exercises1.java
@@ -10,23 +10,23 @@ public class Exercises1 {
         List<Integer> list = List.of(1,2,3,2,1,6,3,4,5,2,3,8,8,9,34,32);
         list.forEach(System.out::println);
 
-        //  TODO
+
         //	Udskriver det største element i listen
         System.out.println("biggest: " + list.stream().max(Integer::compareTo).get());
 
-        //  TODO
+
         //	Afgør og udskriver om alle tallene i listen er mindre end 50
         System.out.println("Er alle større end 50: " + list.stream().allMatch(i -> i < 50));
 
-        //  TODO
+
         // 	Udskriver antallet af lige tal i listen
         System.out.println("Antal Lige tal: " + list.stream().filter(i -> i % 2 == 0).count());
 
-        //  TODO
+
         //	Udskriver antallet af ulige tal i listen
         System.out.println("Antal ulige tal: " + list.stream().filter(i -> i % 2 != 0).count());
 
-        //  TODO
+
         //  Udskriver
         //      Gennemsnittet af tallene i listen
         //      Antallet af tallene i listen
@@ -39,12 +39,12 @@ public class Exercises1 {
         System.out.println("Antal over gennemsnit: " + list.stream().filter(i -> i > stats.getAverage()).count());
         System.out.println("Antal under gennemsnit: " + list.stream().filter(i -> i < stats.getAverage()).count());
 
-        //  TODO
+
         //	Udskriver antallet af gange de forskellige tal forekommer i listen
         Map<Integer, Long> count = list.stream().collect(Collectors.groupingBy(i -> i, Collectors.counting()));
         System.out.println("Antal af hvert tal: " + count);
 
-        //  TODO
+
         //	Udskriver antallet af gange de forskellige tal forekommer i listen i sorteret orden
         count.entrySet().stream().sorted(Map.Entry.comparingByKey()).forEach(System.out::println);
     }
